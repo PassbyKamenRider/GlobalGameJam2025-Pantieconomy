57dd3b35727a54eeed631765d03a2d17920c3617
PassbyKamenRider <PassbyKamenRider@outlook.com>
2025-01-25 22:08:02 -0500

    Add pant rotate

--- Assets/Script/ModelDisplay.cs
+++ Assets/Script/ModelDisplay.cs
@@ -2,9 +2,30 @@ using UnityEngine;
 
 public class ModelDisplay : MonoBehaviour
 {
+    [SerializeField] float amplitude;
+    [SerializeField] float floatSpeed;
+    [SerializeField] float rotateSpeed;
     [SerializeField] Transform model;
+    private Vector3 originPosition;
+
+    private void Start()
+    {
+        originPosition = model.transform.position;
+    }
+
+    private void Update()
+    {
+        RotateModel(rotateSpeed * Time.deltaTime);
+        MoveModel();
+    }
     public void RotateModel(float val)
     {
         model.transform.Rotate(0, val, 0);
+    }
+
+    public void MoveModel()
+    {
+        float yOffset = Mathf.Sin(Time.time * floatSpeed) * amplitude;
+        model.transform.position = new Vector3(originPosition.x, originPosition.y + yOffset, originPosition.z);
     }
 }
