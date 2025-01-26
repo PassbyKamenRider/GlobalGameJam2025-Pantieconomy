d65599c090d2aa643ed81e0c8ff4de4c6b727073
PassbyKamenRider <PassbyKamenRider@outlook.com>
2025-01-26 09:18:24 -0500

    Edit Inventory

--- Assets/Script/InventoryManager.cs
+++ Assets/Script/InventoryManager.cs
@@ -3,40 +3,53 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine.UI;
+using System.Linq;
 
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
-    public List<Item> Items = new List<Item>();
-
-
-
-
-
-
+    public Dictionary<Item, int> items = new();
     public Transform ItemContent;
     public GameObject InventoryItem;
+    public Item[] commonItems, uncommonItems, rareItems, epicItems, legendaryItems;
     private void Awake()
     {
         Instance = this;
     }
 
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            for (int i = 0; i < uncommonItems.Length; i++)
+            {
+                AddItem(uncommonItems[i]);
+            }
+            ListItem();
+        }
+    }
+
     public void AddItem(Item item)
     {
-        Items.Add(item);
+        if (items.ContainsKey(item))
+        {
+            items[item] += 1;
+        }
+        else
+        {
+            items[item] = 1;
+        }
     }
 
     public void RemoveItem(Item item)
     {
-        Items.Remove(item);
+        items.Remove(item);
     }
 
     
 
     public void ListItem()
     {
-
         foreach (Transform item in ItemContent)
         {
             if (item != null)
@@ -45,20 +58,19 @@ public class InventoryManager : MonoBehaviour
             }
         }
 
-
-        foreach (Item item in Items)
+        foreach (var i in items)
         {
-            if (item != null)
+            if (i.Key != null)
             {
                 GameObject obj = Instantiate(InventoryItem, ItemContent);
 
                 var Name = obj.transform.Find("ItemName").GetComponent<Text>();
                 
-                Name.text = item.itemName;
+                Name.text = i.Value.ToString();
 
                 var Icon = obj.transform.Find("ItemIcon").GetComponent<Image>();
 
-                Icon.sprite = item.itemIcon;
+                Icon.sprite = i.Key.itemIcon;
             }
         }
     }
