23a6c172b4c8a41a5f9aa173d213799a75219067
PassbyKamenRider <PassbyKamenRider@outlook.com>
2025-01-25 20:52:38 -0500

    Add player cash

--- Assets/Script/PlayerStats.cs
+++ Assets/Script/PlayerStats.cs
@@ -6,4 +6,21 @@ public class PlayerStats : MonoBehaviour
     public static float dropRate = 50f;
     public static float dropRarity = 5f;
     public static int popPerclick;
+    public static float cash;
+
+    public static void AddCash(float val)
+    {
+        cash += val;
+    }
+
+    public static bool UseCash(float val)
+    {
+        if (cash < val)
+        {
+            return false;
+        }
+
+        cash -= val;
+        return true;
+    }
 }
