c770c3a6965deec89606b3a1af698f2e186b50f3
KkeyserSoze <yifan1106@126.com>
2025-01-26 11:05:31 -0500

    11

--- Assets/OpeningSceneManager.cs
+++ Assets/OpeningSceneManager.cs
@@ -12,7 +12,7 @@ public class OpeningSceneManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.anyKey)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene("MainGame");
         }
